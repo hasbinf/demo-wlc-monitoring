1d16171f717a9f40c5db26036d66fceb8867a01f
hasbinf <hasbinurfadhillah@gmail.com>
2022-03-23 08:28:31 +0000

    update file

--- conf/grafana-entrypoint.sh
+++ conf/grafana-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 # Delete Grafana Title
 # mv /usr/share/grafana/public/img/grafana_typelogo.svg /usr/share/grafana/public/img/grafana_typelogo.svg.bak
-sed -i "s/<title>Grafana/<title>Finiks Solusi Indonesia/g" /usr/share/grafana/public/views/*.html
+sed -i "s/<title>Grafana/<title>Wifi Monitoring (WLC)/g" /usr/share/grafana/public/views/*.html
 
 
 echo "Start Grafana"
